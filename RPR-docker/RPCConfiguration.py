9165a7d6559dd3b88363122c1f00d87ce095df4b
MichaelChestnut <72172361+MichaelChestnut@users.noreply.github.com>
2023-02-26 14:13:58 -0700

    Update RPCConfiguration.py

--- RPR-docker/RPCConfiguration.py
+++ RPR-docker/RPCConfiguration.py
@@ -1,7 +1,7 @@
 resetpin = 4 #IO pin 4 not actually pin 4 (actually pin 7 on Pi4). Change to any GPIO pin
 topic = "RemotePiReset" #topic name
-broker = "ccrisupe3.iot.nau.edu" #broker name or IP address
-client = "ccriiobe3.iot.nau.edu" #client name or IP address
+broker = "test.mosquitto.org" #broker name or IP address
+client = "192.168.1.244" #client name or IP address
 cacert = "/SOME/PATH/TO/YOUR_CAFILE.crt" #path to cafile
 certfile = "/SOME/PATH/TO/YOUR_CERTFILE.crt" #path to certfile
 keyfile = "/SOME/PATH/TO/YOUR_KEYFILE.key" #path to keyfile
