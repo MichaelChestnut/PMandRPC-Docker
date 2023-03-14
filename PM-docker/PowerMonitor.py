8069a86aaed2d8e960c408255c2e60b3ad2da5f0
MichaelChestnut <72172361+MichaelChestnut@users.noreply.github.com>
2023-03-14 14:33:09 -0700

    Update PowerMonitor.py

--- PM-docker/PowerMonitor.py
+++ PM-docker/PowerMonitor.py
@@ -8,16 +8,23 @@ from adafruit_ina219 import ADCResolution, BusVoltageRange, INA219
 import paho.mqtt.client as mqtt
 import logging
 import PMConfiguration as config
+import csv
+import os.path
 
 #enable logging
 logging.basicConfig(level=logging.DEBUG)
 
 #import variables from config file
 topic = config.topic
+Port = config.port
+DataPath = config.datastorage
 Broker = config.broker
 Load1 = config.load1
 Load2 = config.load2
 Load3 = config.load3
+PrintLoad1 = config.printload1
+PrintLoad2 = config.printload2
+PrintLoad3 = config.printload3
 CA_Certs = config.cacert
 Certfile = config.certfile
 Keyfile = config.keyfile
@@ -74,7 +81,7 @@ client.will_set(topic, b'Monitoring script has terminated')
 #               keyfile=Keyfile)
 
 # create connection, the three parameters are broker address, broker port numbe>
-client.connect(Broker, 1883, 60)
+client.connect(Broker, Port, 60)
 
 
 i2c_bus = board.I2C()
@@ -96,6 +103,28 @@ ina2.bus_voltage_range = BusVoltageRange.RANGE_16V
 ina3.bus_adc_resolution = ADCResolution.ADCRES_12BIT_32S
 ina3.shunt_adc_resolution = ADCResolution.ADCRES_12BIT_32S
 ina3.bus_voltage_range = BusVoltageRange.RANGE_16V
+
+header = ['DateAndTime', 'LoadName', 'ShuntVoltage', 'LoadVoltage', 'Current', 'Power']
+
+timestr = time.strftime("%Y%m%d") #current date for filename
+base = "PowerMonitor-" #base name of file
+extension = ".csv" #.csv extension for filename
+filename = base + timestr + extension #combine into filename
+datastorage = DataPath + filename #append filename to end of DataPath to create one variable
+
+testforfile = os.path.exists(datastorage)
+
+if(testforfile):
+
+    file = open(datastorage, 'a')
+    writer = csv.writer(file)
+
+else:
+    file = open(datastorage, 'w')
+    writer = csv.writer(file)
+
+    # write the header
+    writer.writerow(header)
 
 
 # measure and display loop
@@ -127,17 +156,49 @@ while True:
     logging.debug("\n\npublishing data to topic: {}\n".format(topic))
 
     logging.debug(str(currentDandT))
-    logging.debug(Str1.format((Load1),(shunt_voltage1),(bus_voltage1),(current1/1000),(power1)))
-    logging.debug(Str2.format((Load2),(shunt_voltage2),(bus_voltage2),(current2/1000),(power2)))
-    logging.debug(Str3.format((Load3),(shunt_voltage3),(bus_voltage3),(current3/1000),(power3)))
+    if(PrintLoad1):
+        logging.debug(Str1.format((Load1),(shunt_voltage1),(bus_voltage1),(current1/1000),(power1)))
+    if(PrintLoad2):
+        logging.debug(Str2.format((Load2),(shunt_voltage2),(bus_voltage2),(current2/1000),(power2)))
+    if(PrintLoad3):
+        logging.debug(Str3.format((Load3),(shunt_voltage3),(bus_voltage3),(current3/1000),(power3)))
     logging.debug("-"*100)
 
 #publish data to topic
     client.publish(topic, Str4)
     client.publish(topic, str(currentDandT))
-    client.publish(topic, Str1.format((Load1),(shunt_voltage1),(bus_voltage1),(current1/1000),(power1)))
-    client.publish(topic, Str2.format((Load2),(shunt_voltage2),(bus_voltage2),(current2/1000),(power2)))
-    client.publish(topic, Str3.format((Load3),(shunt_voltage3),(bus_voltage3),(current3/1000),(power3)))
+    if(PrintLoad1):
+        client.publish(topic, Str1.format((Load1),(shunt_voltage1),(bus_voltage1),(current1/1000),(power1)))
+    if(PrintLoad2):
+        client.publish(topic, Str2.format((Load2),(shunt_voltage2),(bus_voltage2),(current2/1000),(power2)))
+    if(PrintLoad3):
+        client.publish(topic, Str3.format((Load3),(shunt_voltage3),(bus_voltage3),(current3/1000),(power3)))
     client.publish(topic, Str4)
     client.publish(topic, Str4)
+
+
+#write data to csv file
+    data1 = [currentDandT, Load1, round(shunt_voltage1, 6), round(bus_voltage1, 3), round(current1/1000, 6), round(power1, 6)]
+    data2 = [currentDandT, Load2, round(shunt_voltage2, 6), round(bus_voltage2, 3), round(current2/1000, 6), round(power2, 6)]
+    data3 = [currentDandT, Load3, round(shunt_voltage3, 6), round(bus_voltage3, 3), round(current3/1000, 6), round(power3, 6)]
+    if(PrintLoad1):
+        writer.writerow(data1)
+    if(PrintLoad2):
+        writer.writerow(data2)
+    if(PrintLoad3):
+        writer.writerow(data3)
+
+    file.flush() #flush data to disk
+    timestr = time.strftime("%Y%m%d") #update current date for newfilename
+    newfilename = base + timestr + extension #combine into new filename
+
+#test if it is a new day, i.e. newfile name has the new date in it
+    if(newfilename != filename):
+        f.close()       #close old file
+        filename = newfilename  #set filename to new date filename
+        datastorage = DataPath + filename #update variable
+        file = open(datastorage, 'w')   #open new file with current date
+        writer = csv.writer(file)
+        writer.writerow(header) #write the header to the new file
+
     time.sleep(Sleeptime)
