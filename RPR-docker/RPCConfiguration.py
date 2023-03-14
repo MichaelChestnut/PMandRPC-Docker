9e399c0b34052d4bcf1499af6e0e82957d4a4489
MichaelChestnut <72172361+MichaelChestnut@users.noreply.github.com>
2023-03-14 14:54:29 -0700

    Update RPCConfiguration.py

--- RPR-docker/RPCConfiguration.py
+++ RPR-docker/RPCConfiguration.py
@@ -1,7 +1,7 @@
-resetpin = 4 #IO pin 4 not actually pin 4 (actually pin 7 on Pi4). Change to any GPIO pin
-topic = "RemotePiReset" #topic name
-broker = "test.mosquitto.org" #broker name or IP address
-client = "192.168.1.244" #client name or IP address
+topic = "YOUR TOPIC NAME" #topic name
+port = "YOUR_MQTT_PORT_NUMBER" #port to be used by mqtt
+broker = "YOUR BROKER IP/HOSTNAME" #broker DNS name or IP address
+client = "YOUR CLIENT IP/HOSTNAME" #client DNS name or IP address
 cacert = "/SOME/PATH/TO/YOUR_CAFILE.crt" #path to cafile
 certfile = "/SOME/PATH/TO/YOUR_CERTFILE.crt" #path to certfile
 keyfile = "/SOME/PATH/TO/YOUR_KEYFILE.key" #path to keyfile
